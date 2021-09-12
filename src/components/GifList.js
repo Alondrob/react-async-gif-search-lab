d3d4d4c36f84bfec740b92db9ee997d91a8f995b
Alondrob <alondrob@gmail.com>
2021-09-11 22:31:55 -0400

    Done.

---
+++ src/components/GifList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class GifList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default GifList
